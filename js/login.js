fc138f2c0a149738e4634602d7457ab78308c24b
DanielPot217 <danielp0517@gmail.com>
2025-03-14 15:28:09 -0400

    Bug Fixes  - Fixed Contact Icon Positioning  - Fixed Navigation Bar Item Spacing  - Fixed HTML Validation  - Fixed CSS Validation
     - Added Missing Button Animations(Support Page Submit)
     - Added Language Declaration
     - Added Documentaion

--- js/login.js
+++ js/login.js
@@ -1,9 +1,11 @@
 "use strict";
 
+//On click event that is called when the login button 
 document.getElementById("login-submit").onclick = function(event){
 
     event.preventDefault();
 
+    //Check what value is entered and navigates the user to the specified page
     let email = document.getElementsByName("login-email")[0].value;
 
     if(email === "Admin" || email === "admin")
